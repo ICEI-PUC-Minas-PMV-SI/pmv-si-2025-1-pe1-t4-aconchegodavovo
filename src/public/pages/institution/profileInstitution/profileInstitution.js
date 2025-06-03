2921bfa1978ea342782cb4a535a6eb35ced56731
Raissa <raissa.barbieri@begrowth.com.br>
2025-06-03 19:38:15 -0300

    feat: add profile instituion js

---
+++ src/public/pages/institution/profileInstitution/profileInstitution.js
@@ -0,0 +1,21 @@
+const API_URL = 'http://localhost:3000/institutionProfile';
+
+const nomeEl = document.getElementById('nome-instituicao');
+const descricaoEl = document.getElementById('descricao-instituicao');
+const fotoEl = document.getElementById('foto-instituicao');
+
+async function carregarPerfil() {
+  try {
+    const res = await fetch(API_URL);
+    const perfil = await res.json(); 
+
+    nomeEl.textContent = perfil.nome;
+    descricaoEl.textContent = perfil.descricao;
+    fotoEl.src = perfil.foto;
+  } catch (err) {
+    console.error('Erro ao carregar perfil:', err);
+    nomeEl.textContent = 'Erro ao carregar dados';
+  }
+}
+
+carregarPerfil();
