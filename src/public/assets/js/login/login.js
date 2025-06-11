86cb852f0ea36b0f36b3697343433fe949936df6
CamilaSGom <camilasgomes00@gmail.com>
2025-06-10 21:35:58 -0300

    chore: fix login

--- src/public/assets/js/login/login.js
+++ src/public/assets/js/login/login.js
@@ -4,8 +4,8 @@ const loginForm = document.getElementById('loginForm');
 loginForm.addEventListener('submit', function (e) {
     e.preventDefault(); // evita o envio padrão do formulário
 
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value.trim();
 
     // Faz uma requisição GET para buscar os usuários
     fetch('http://localhost:3000/usuarios')
@@ -21,12 +21,19 @@ loginForm.addEventListener('submit', function (e) {
 
             // Verifica se a senha confere
             if (user.senha === senha) {
-                window.location.href = '../../../pages/donor/listInstitutions/listInstitutions.html';
+                // Verifica se o usuário é uma instituição
+                if (user.instituicao === true) {
+                    window.location.href = '../../../pages/institution/historyReceipts/historyReceipts.html';
+                } else {
+                    // Caso seja um doador
+                    window.location.href = '../../../pages/donor/listInstitutions/listInstitutions.html';
+                }
             } else {
                 alert('Senha incorreta!');
             }
         })
         .catch(error => {
             console.error('Erro ao fazer login:', error);
+            alert('Ocorreu um erro. Tente novamente mais tarde.');
         });
 });
